f5c3123abce264f37e951e335248073d4d5b2ae8
Matt Kearns <matt.kearns@ccri.com>
2022-07-22 08:20:50 -0400

    update __init__.py

--- geomesa-spark/geomesa_pyspark/src/main/python/geomesa_pyspark/__init__.py
+++ geomesa-spark/geomesa_pyspark/src/main/python/geomesa_pyspark/__init__.py
@@ -37,6 +37,7 @@ def configure(jars=[], packages=[], files=[], spark_home=None, spark_master='yar
     import geomesa_pyspark.types
     import geomesa_pyspark.constructors
     import geomesa_pyspark.accessors
+    import geomesa_pyspark.spatial
 
     # Need differential behavior based for <= Spark 2.0.x, Spark 2.1.0
     #  is the fist release to provide the module __version__ attribute
